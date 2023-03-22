62e55de9c03cc7034697f4f27739b7cf5e878c1f
Emery Berger <emery.berger@gmail.com>
2023-03-22 11:10:32 -0400

    Commentated.

--- src/chatdbg/chatdbg_why.py
+++ src/chatdbg/chatdbg_why.py
@@ -2,53 +2,58 @@ import openai
 import openai_async
 import os
 import sys
-
 import textwrap
 
-def word_wrap_except_code_blocks(text):
-    paragraphs = text.split('\n\n') # Split text into paragraphs
-    wrapped_paragraphs = []
-    in_code_block = False
+def word_wrap_except_code_blocks(text: str) -> str:
+    """Wraps text except for code blocks.
 
+    Splits the text into paragraphs and wraps each paragraph,
+    except for paragraphs that are inside of code blocks denoted 
+    by ` ``` `. Returns the updated text.
+
+    Args:
+        text: The text to wrap.
+
+    Returns:
+        The wrapped text.
+    """
+    # Split text into paragraphs
+    paragraphs = text.split('\n\n')
+    wrapped_paragraphs = []
+    # Check if currently in a code block.
+    in_code_block = False
+    # Loop through each paragraph and apply appropriate wrapping.
     for paragraph in paragraphs:
+        # If this paragraph starts and ends with a code block, add it as is.
         if paragraph.startswith('```') and paragraph.endswith('```'):
-            # If this is a code block, skip wrapping and add it as is
             wrapped_paragraphs.append(paragraph)
             continue
-
+        # If this is the beginning of a code block add it as is.
         if paragraph.startswith('```'):
-            # Beginning of a code block
             in_code_block = True
             wrapped_paragraphs.append(paragraph)
             continue
-
+        # If this is the end of a code block stop skipping text.
         if paragraph.endswith('```'):
-            # End of a code block
             in_code_block = False
             wrapped_paragraphs.append(paragraph)
             continue
-
+        # If we are currently in a code block add the paragraph as is.
         if in_code_block:
-            # If in a code block, add paragraph as is
             wrapped_paragraphs.append(paragraph)
         else:
-            # If not in a code block, wrap paragraph and add to list
-            wrapped_paragraph = textwrap.fill(paragraph) # , width=80)
+            # Otherwise, apply text wrapping to the paragraph.
+            wrapped_paragraph = textwrap.fill(paragraph)
             wrapped_paragraphs.append(wrapped_paragraph)
-
     # Join all paragraphs into a single string
     wrapped_text = '\n\n'.join(wrapped_paragraphs)
-
     return wrapped_text
-
 
 async def why(self, arg):
     user_prompt = 'Explain what the root cause of this error is, given the following source code and traceback, and propose a fix.'
     user_prompt += '\n'
     user_prompt += 'source code:\n```\n'
-
     stack_trace = ''
-    
     stack_frames = len(self.stack)
     try:
         import sys
@@ -57,7 +62,6 @@ async def why(self, arg):
     except:
         print("The command 'why' only works when there is an uncaught exception. Try running 'python3 -m chatdbg -c continue'.")
         return
-
     # print(dir(self))
     for frame_lineno in self.stack:
         import inspect
@@ -85,7 +89,6 @@ async def why(self, arg):
     user_prompt += 'stack trace:\n'
     user_prompt += f'```\n{stack_trace}```\n'
     user_prompt += f'Exception: {exception_name} ({exception_value})\n'
-
     # print(user_prompt)
     import httpx
     text = ''
@@ -100,6 +103,6 @@ async def why(self, arg):
         import sys
         sys.exit(1)
     except Exception as e:
-        print(f"EXCEPTION {e}")
+        print(f'EXCEPTION {e}')
         pass
     print(word_wrap_except_code_blocks(text))

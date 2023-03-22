f9230672cba614ac7c9eaf459e3842d49dc89f96
Emery Berger <emery.berger@gmail.com>
2023-03-21 21:15:07 -0400

    Added word wrapping of response (except for code blocks).

--- src/chatdbg/chatdbg_why.py
+++ src/chatdbg/chatdbg_why.py
@@ -3,6 +3,45 @@ import openai_async
 import os
 import sys
 
+import textwrap
+
+def word_wrap_except_code_blocks(text):
+    paragraphs = text.split('\n\n') # Split text into paragraphs
+    wrapped_paragraphs = []
+    in_code_block = False
+
+    for paragraph in paragraphs:
+        if paragraph.startswith('```') and paragraph.endswith('```'):
+            # If this is a code block, skip wrapping and add it as is
+            wrapped_paragraphs.append(paragraph)
+            continue
+
+        if paragraph.startswith('```'):
+            # Beginning of a code block
+            in_code_block = True
+            wrapped_paragraphs.append(paragraph)
+            continue
+
+        if paragraph.endswith('```'):
+            # End of a code block
+            in_code_block = False
+            wrapped_paragraphs.append(paragraph)
+            continue
+
+        if in_code_block:
+            # If in a code block, add paragraph as is
+            wrapped_paragraphs.append(paragraph)
+        else:
+            # If not in a code block, wrap paragraph and add to list
+            wrapped_paragraph = textwrap.fill(paragraph) # , width=80)
+            wrapped_paragraphs.append(wrapped_paragraph)
+
+    # Join all paragraphs into a single string
+    wrapped_text = '\n\n'.join(wrapped_paragraphs)
+
+    return wrapped_text
+
+
 async def why(self, arg):
     user_prompt = 'Explain what the root cause of this error is, given the following source code and traceback, and propose a fix.'
     user_prompt += '\n'
@@ -59,4 +98,4 @@ async def why(self, arg):
     except Exception as e:
         print(f"EXCEPTION {e}")
         pass
-    print(text)
+    print(word_wrap_except_code_blocks(text))

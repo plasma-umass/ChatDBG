9602b13ff0deb6447a7c88f58e47968f3e10dd58
Emery Berger <emery.berger@gmail.com>
2023-03-22 19:39:19 -0400

    Removed cruft.

--- src/chatdbg/__main__.py
+++ src/chatdbg/__main__.py
@@ -1,20 +1,6 @@
 import os
 
 from . import chatdbg
-    
-def api_key() -> str:
-    """
-    Get the API key from the environment variable 'OPENAI_API_KEY'.
-    
-    :return: The value of the environment variable 'OPENAI_API_KEY'.
-    :rtype: str
-    """
-    key = ''
-    try:
-        key = os.environ['OPENAI_API_KEY']
-    except:
-        pass
-    return key
 
 def main():
     chatdbg.main()

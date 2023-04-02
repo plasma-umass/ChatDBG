a7a1ba2a78aaf769237cb8fce1aacd710eda9d3d
Emery Berger <emery.berger@gmail.com>
2023-04-01 20:56:28 -0400

    Added.

---
+++ test/test-failed-assert.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+float fact(float n) {
+  auto x = 1.0;
+  for (auto i = 0.0; i < n; i++) {
+    x *= i;
+  }
+  assert(x != 0.0);
+  return x;
+}
+
+
+int main()
+{
+  std::cout << fact(100) << std::endl;
+  return 0;
+}

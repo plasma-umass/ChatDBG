9d91198acbdb1e51112f75b48e5f79f6d039f1e8
Emery Berger <emery.berger@gmail.com>
2023-09-30 10:38:33 -0400

    Working on curl.

--- src/chatdbg/build-chatdbg.bat
+++ src/chatdbg/build-chatdbg.bat
@@ -1,2 +1,2 @@
-cl /MD /Zi /c chatdbg.cpp
-link /DLL chatdbg.obj "C:\Program Files (x86)\Windows Kits\10\Debuggers\lib\arm64\dbgeng.lib" "C:\Program Files (x86)\Windows Kits\10\Debuggers\lib\arm64\dbghelp.lib"
+cl /EHsc /MD /Zi /c /I. /std:c++20 chatdbg.cpp
+link /DLL chatdbg.obj "C:\Program Files (x86)\Windows Kits\10\Debuggers\lib\arm64\dbgeng.lib" "C:\Program Files (x86)\Windows Kits\10\Debuggers\lib\arm64\dbghelp.lib" libcurl.lib

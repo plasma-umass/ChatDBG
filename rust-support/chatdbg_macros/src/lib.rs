0ff8bd6d7232eb48e95d1ee43430f251dc829bb1
Noah Lev <camelidcamel@gmail.com>
2023-10-02 18:35:58 -0400

    Improve macro error handling

--- rust-support/chatdbg_macros/src/lib.rs
+++ rust-support/chatdbg_macros/src/lib.rs
@@ -3,13 +3,17 @@ use quote::quote;
 use syn::{parse, ItemFn};
 
 #[proc_macro_attribute]
-pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn main(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let item = match parse(input) {
+        Ok(i) => i,
+        Err(_) => return quote! {}.into(),
+    };
     let ItemFn {
         attrs,
         vis,
         sig,
         block,
-    } = parse(item).unwrap();
+    } = item;
     let stmts = &block.stmts;
     quote! {
         #(#attrs)* #vis #sig {

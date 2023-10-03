b4ea8a701cec896520d5acb36440e50d211e0206
Nicolas van Kempen <nvankempen@fb.com>
2023-10-03 09:25:42 -0700

    Fix #21, remove dependencies

--- src/chatdbg/chatdbg_utils.py
+++ src/chatdbg/chatdbg_utils.py
@@ -1,8 +1,8 @@
-import openai
-import openai_async
 import os
 import sys
 import textwrap
+
+import openai
 
 
 def get_model() -> str:
@@ -165,39 +165,20 @@ async def explain(
         print(user_prompt)
         return
 
-    if not "OPENAI_API_KEY" in os.environ:
-        print(
-            "You need a valid OpenAI key to use ChatDBG. You can get a key here: https://openai.com/api/"
-        )
-        print("Set the environment variable OPENAI_API_KEY to your key value.")
-        return
-
     model = get_model()
     if not model:
         return
 
     try:
-        completion = await openai_async.chat_complete(
-            openai.api_key,
-            timeout=30,
-            payload={
-                "model": f"{model}",
-                "messages": [{"role": "user", "content": user_prompt}],
-            },
+        completion = openai.ChatCompletion.create(
+            model=model,
+            request_timeout=30,
+            messages=[{"role": "user", "content": user_prompt}],
         )
-        json_payload = completion.json()
-        text = json_payload["choices"][0]["message"]["content"]
-    except (openai.error.AuthenticationError, httpx.LocalProtocolError, KeyError):
-        # Something went wrong.
-        print()
+        text = completion.choices[0].message.content
+        print(chatdbg_utils.word_wrap_except_code_blocks(text))
+    except openai.error.AuthenticationError:
         print(
             "You need a valid OpenAI key to use ChatDBG. You can get a key here: https://openai.com/api/"
         )
         print("Set the environment variable OPENAI_API_KEY to your key value.")
-        import sys
-
-        sys.exit(1)
-    except Exception as e:
-        print(f"EXCEPTION {e}, {type(e)}")
-        pass
-    print(word_wrap_except_code_blocks(text))

ba3cc5b55d0cad2a1a859362d8be154a532d0274
Stephen Freund <sfreund@williams.edu>
2024-02-14 18:16:03 +0000

    initial port of new pdb integration

---
+++ test/python/marbles.py
@@ -0,0 +1,18 @@
+from datascience import *
+from cs104 import *
+
+def make_marble_bag():
+    table = Table().read_table('marble-sample.csv')
+    return table.column('color')
+
+def ratio(x,y):
+    return x / y
+
+def ratio_blue_to_red(sample):
+    blues = np.count_nonzero(sample == 'B') 
+    reds = np.count_nonzero(sample == 'r')
+    return ratio(blues, reds)
+
+marbles = make_marble_bag()
+if 'R' in marbles:
+    print(ratio_blue_to_red(marbles))
---
+++ test/python/mean.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+from datascience import *
+from cs104 import *
+
+def make_marble_bag():
+    table = Table().read_table('marble-sample.csv')
+    return table.column('color')
+
+observed_marbles = make_marble_bag()
+
+def percent_blue(sample):
+    return np.count_nonzero(sample == 'B') / len(sample)
+
+def main():
+
+    num_trials = 5
+        
+    stats = bootstrap_statistic(observed_marbles,
+                                percent_blue,
+                                num_trials)
+
+    assert np.isclose(np.mean(stats), 0.7)
+
+main()

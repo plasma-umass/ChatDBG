251d8ca46d216d2ea7b30fba98dd82a4e20ce27c
Kyla Levin <62115756+ravenblood000@users.noreply.github.com>
2024-05-28 17:10:41 +0000

    remove

--- samples/python/bootstrap2.py
+++ samples/python/bootstrap2.py
@@ -1,11 +1,13 @@
 from datascience import *
 from ds101 import *
+import pdb
 
 def make_marble_sample():
     table = Table().read_table('marble-sample.csv')
     return table.column('color')
 
 def proportion_blue(sample):
+    pdb.set_trace()
     return np.count_nonzero(sample == 'B') / len(sample)
 
 def resampled_stats(observed_marbles, num_trials):
@@ -15,7 +17,10 @@ def resampled_stats(observed_marbles, num_trials):
     assert len(stats) == num_trials
     return stats
 
-observed_marbles = make_marble_sample()
-stats = resampled_stats(observed_marbles, 5)
+def compute_the_stat():
+    observed_marbles = make_marble_sample()
+    
+    return resampled_stats(observed_marbles, 1000)
 
-assert np.isclose(np.mean(stats), 0.7)
+stats = compute_the_stat()
+assert np.isclose(np.mean(stats), 0.5)

196013bbae4e073091167fb2c4a61aa880a1a111
Kyla Levin <62115756+khlevin@users.noreply.github.com>
2024-09-11 12:26:15 -0400

    Update assistant.py
    Doesn't work with 4o

--- src/chatdbg/assistant/assistant.py
+++ src/chatdbg/assistant/assistant.py
@@ -316,10 +316,6 @@ class Assistant:
         return litellm.completion(
             model=self._model,
             messages=self._conversation,
-            tools=[
-                {"type": "function", "function": f["schema"]}
-                for f in self._functions.values()
-            ],
             timeout=self._timeout,
             logger_fn=self._logger,
             stream=stream,

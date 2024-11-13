e64b14d727429b2e889871c9cb2c71a9347d3dd1
Kyla Levin <62115756+ravenblood000@users.noreply.github.com>
2024-11-13 17:31:19 +0000

    import

--- src/chatdbg/util/exit_message.py
+++ src/chatdbg/util/exit_message.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def print_exit_message(*args: Any, **kwargs: Any) -> None:
     print("Thank you for using ChatDBG!")
     print("Share your success stories here: github.com/plasma-umass/ChatDBG/issues/53")

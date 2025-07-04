3d5315ff321b90c0ec75e703e87a663b70d3b3d7
Julien Audet <julien.audet@ubisoft.com>
2025-07-04 13:09:10 -0400

    refactor(argparser): :recycle: extract help message for reuse

--- src/chatdbg/__main__.py
+++ src/chatdbg/__main__.py
@@ -1,25 +1,10 @@
-import ipdb
-from chatdbg.chatdbg_pdb import ChatDBG
-from chatdbg.util.config import chatdbg_config
 import sys
 
-_usage = """\
-usage: python -m ipdb [-m] [-c command] ... pyfile [arg] ...
+import ipdb
 
-Debug the Python program given by pyfile.
-
-Initial commands are read from .pdbrc files in your home directory
-and in the current directory, if they exist.  Commands supplied with
--c are executed after commands from .pdbrc files.
-
-To let the script run until an exception occurs, use "-c continue".
-To let the script run up to a given line X in the debugged file, use
-"-c 'until X'"
-
-Option -m is available only in Python 3.7 and later.
-
-ChatDBG-specific options may appear anywhere before pyfile:
-"""
+from chatdbg.chatdbg_pdb import ChatDBG
+from chatdbg.util.config import chatdbg_config
+from chatdbg.util.help import print_help
 
 
 def main() -> None:
@@ -28,9 +13,7 @@ def main() -> None:
     args = chatdbg_config.parse_user_flags(sys.argv[1:])
 
     if "-h" in args or "--help" in args:
-        print(_usage)
-        print(chatdbg_config.user_flags_help())
-        sys.exit()
+        print_help()
 
     sys.argv = [sys.argv[0]] + args
 
